72ec59603edda202de349d1120df6b9c381ea4bd
Kendall Harter <kendallremna@protonmail.com>
2025-09-28 04:59:50 -0700

    Add some benchmarking

--- tests/basic_tests.cpp
+++ tests/basic_tests.cpp
@@ -1,6 +1,7 @@
 #include "cpp_dyn/cpp_dyn.hpp"
 #include "test_common.hpp"
 
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Basic functionality", "[basic]")
@@ -22,4 +23,36 @@ TEST_CASE("Basic functionality", "[basic]")
    REQUIRE(khct::call(trait5, trait5.get_secondary_noise) == "(none)");
    khct::call(trait5, trait5.get_louder_twice);
    REQUIRE(khct::call(trait5, trait5.volume, 1) == 4);
+}
+
+TEST_CASE("Benchmark basic", "[!benchmark]")
+{
+   BENCHMARK("Stack allocated, external vtable")
+   {
+      auto trait = khct::owning_dyn<noise_trait, khct::owning_dyn_options{.stack_size = 8}>(cow{});
+      trait.call(trait.get_louder);
+      return trait.call(trait.volume);
+   };
+
+   BENCHMARK("Stack allocated, inline vtable")
+   {
+      auto trait
+         = khct::owning_dyn<noise_trait, khct::owning_dyn_options{.store_vtable_inline = true, .stack_size = 8}>(cow{});
+      trait.call(trait.get_louder);
+      return trait.call(trait.volume);
+   };
+
+   BENCHMARK("Heap allocated, external vtable")
+   {
+      auto trait = khct::owning_dyn<noise_trait, khct::owning_dyn_options{.store_vtable_inline = false}>(cow{});
+      trait.call(trait.get_louder);
+      return trait.call(trait.volume);
+   };
+
+   BENCHMARK("Heap allocated, inline vtable")
+   {
+      auto trait = khct::owning_dyn<noise_trait, khct::owning_dyn_options{.store_vtable_inline = true}>(cow{});
+      trait.call(trait.get_louder);
+      return trait.call(trait.volume);
+   };
 }
