d885e263c1d3c747d0556c716512f15b2b58a763
Kendall Harter <kendallremna@protonmail.com>
2025-09-28 04:30:40 -0700

    Small adjustment to testing

--- tests/compile_time_tests.cpp
+++ tests/compile_time_tests.cpp
@@ -85,7 +85,7 @@ int main()
    const auto trait = khct::owning_dyn<noise_trait, khct::owning_dyn_options{.stack_size = 8}>(cow{});
    assert(khct::call(trait, trait.volume) == 1);
 
-   auto trait2 = khct::owning_dyn<noise_trait>(dog{});
+   auto trait2 = khct::owning_dyn<noise_trait, khct::owning_dyn_options{.store_vtable_inline = true}>(dog{});
    khct::call(trait2, trait2.get_louder);
    auto trait3 = std::move(trait2);
    assert(khct::call(trait3, trait3.volume) == 18);
